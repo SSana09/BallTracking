874f3d6388a6e866bf4eaffdf1099472b70a120c
SSana09 <ssantimano531@student.fuhsd.org>
2025-03-04 13:26:12 -0800

    edited method

--- src/Filters/ColorMask.java
+++ src/Filters/ColorMask.java
@@ -1,9 +1,10 @@
 package Filters;
 
+import Interfaces.Interactive;
 import Interfaces.PixelFilter;
 import core.DImage;
 
-public class ColorMask implements PixelFilter {
+public class ColorMask implements PixelFilter, Interactive {
 
     private static int threshold = 50;
 
@@ -15,18 +16,14 @@ public class ColorMask implements PixelFilter {
         short[][] red = img.getRedChannel();
         short[][] blue = img.getBlueChannel();
         short[][] green = img.getGreenChannel();
+
         for (int r=0; r<red.length; r++) {
             for (int c=0; c<red[r].length; c++) {
                 int redDiff = targetRed-red[r][c];
                 int greenDiff = targetGreen-green[r][c];
                 int blueDiff = targetBlue-blue[r][c];
                 int diff = (int) Math.sqrt(redDiff*redDiff + greenDiff*greenDiff + blueDiff*blueDiff);
-                if (diff<threshold) {
-                    red[r][c] = 255;
-                    green[r][c] = 255;
-                    blue[r][c] = 255;
-                }
-                else {
+                if (diff>threshold) {
                     red[r][c] = 0;
                     green[r][c] = 0;
                     blue[r][c] = 0;
@@ -36,4 +33,22 @@ public class ColorMask implements PixelFilter {
         img.setColorChannels(red, green, blue);
         return img;
     }
+
+    public void mouseClicked(int mouseX, int mouseY, DImage img) {
+        short[][] red = img.getRedChannel();
+        short[][] blue = img.getBlueChannel();
+        short[][] green = img.getGreenChannel();
+        targetRed = red[mouseY][mouseX];
+        targetGreen = green[mouseY][mouseX];
+        targetBlue = blue[mouseY][mouseX];
+    }
+
+    @Override
+    public void keyPressed(char key) {
+        if (key=='+') threshold+=5;
+        if (key == '-') {
+            if (threshold>=5) threshold-=5;
+        }
+    }
+
 }
