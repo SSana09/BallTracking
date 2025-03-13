9d737c276673a5a883e5ac22cf8d2242f17ca1af
Sana Santimano <ssantimano531@student.fuhsd.org>
2025-03-13 14:33:50 -0700

    changed-ish

--- src/Filters/ColorMask.java
+++ src/Filters/ColorMask.java
@@ -4,99 +4,113 @@ import Interfaces.Interactive;
 import Interfaces.PixelFilter;
 import core.DImage;
 
-import java.awt.*;
-
 public class ColorMask implements PixelFilter, Interactive {
 
-    private static float hueThresh = (float) 0.0278;
-    private static float satThresh = (float) 0.02;
-    private static float valThresh = (float) 0.02;
+    //mini squares, dlt if surrounded by black, or on 3 sides
+    //100x100 squares (testable)
 
-    private int targetRed1 = 255;
-    private int targetBlue1 = 5;
-    private int targetGreen1 = 0; // red
+    private double threshold = 0.35;
 
-    private int targetRed2 = 255;
-    private int targetBlue2 = 0;
-    private int targetGreen2 = 165; // orange
+    private short[] green1 = {107, 163, 88};
+    private short[] green2 = {73, 94, 53};
+    private short[] green3 = {54, 91, 39};
+    private short[][] setGreens = {green1, green2, green3};
 
-    private int targetRed3 = 0;
-    private int targetBlue3 = 255;
-    private int targetGreen3 = 5; // blue
+    private short[] red1 = {250, 108, 88};
+    private short[] red2 = {188, 77, 57};
+    private short[] red3 = {195, 80, 61};
+    private short[][] setReds = {red1, red2, red3};
+
+    private short[] blue1 = {54, 68, 133};
+    private short[] blue2 = {25, 26, 83};
+    private short[] blue3 = {56, 83, 157};
+    private short[][] setBlues = {blue1, blue2, blue3};
 
     public DImage processImage(DImage img) {
-        float[] targetHSB1 = new float[3];
-        float[] targetHSB2 = new float[3];
-        float[] targetHSB3 = new float[3];
-        targetHSB1 = Color.RGBtoHSB(targetRed1, targetGreen1, targetBlue1, targetHSB1);
-        targetHSB2 = Color.RGBtoHSB(targetRed2, targetGreen2, targetBlue2, targetHSB2);
-        targetHSB3 = Color.RGBtoHSB(targetRed3, targetGreen3, targetBlue3, targetHSB3);
-
         short[][] red = img.getRedChannel();
         short[][] blue = img.getBlueChannel();
         short[][] green = img.getGreenChannel();
-
-        for (int r = 0; r <red.length; r++) {
+        //rgb
+        for (int r = 0; r<red.length; r++) {
             for (int c = 0; c<red[r].length; c++) {
-                int redPix = red[r][c];
-                int greenPix = green[r][c];
-                int bluePix = blue[r][c];
-                float[] hsb = new float[3];
-                hsb = Color.RGBtoHSB(redPix, greenPix, bluePix, hsb);
-
-                if (hsb[0]<=targetHSB1[0]+hueThresh && hsb[0]>=targetHSB1[0]-hueThresh) {
-                    if (hsb[1]<=targetHSB1[1]+satThresh && hsb[1]>=targetHSB1[1]-satThresh) {
-                        if (hsb[2]<=targetHSB1[2]+valThresh && hsb[2]>=targetHSB1[2]-valThresh) {
-                            red[r][c] = 255;
-                            green[r][c] = 0;
-                            blue[r][c] = 0;
-                        }
-                    }
-                }//red
-
-                else if (hsb[0]<=targetHSB2[0]+hueThresh && hsb[0]>=targetHSB2[0]-hueThresh) {
-                    if (hsb[1]<=targetHSB2[1]+satThresh && hsb[1]>=targetHSB2[1]-satThresh) {
-                        if (hsb[2]<=targetHSB2[2]+valThresh && hsb[2]>=targetHSB2[2]-valThresh) {
-                            red[r][c] = 255;
-                            green[r][c] = 165;
-                            blue[r][c] = 0;
-                        }
-                    }
-                }//orange
-
-                else if (hsb[0]<=targetHSB3[0]+hueThresh && hsb[0]>=targetHSB3[0]-hueThresh) {
-                    if (hsb[1]<=targetHSB3[1]+satThresh && hsb[1]>=targetHSB3[1]-satThresh) {
-                        if (hsb[2]<=targetHSB3[2]+valThresh && hsb[2]>=targetHSB3[2]-valThresh) {
-                            red[r][c] = 0;
-                            green[r][c] = 5;
-                            blue[r][c] = 255;
-                        }
-                    }
-                }//blue
-
-                else {
-                    red[r][c] = 0;
-                    green[r][c] = 0;
-                    blue[r][c] = 0;
+                double bg, br;
+                if (green[r][c]<=0) {
+                    green[r][c] = 1;
+                }
+                if (red[r][c]<=0) {
+                    red[r][c] = 1;
                 }
 
+                bg = blue[r][c]/green[r][c];
+                br = blue[r][c]/red[r][c];
+
+                for (int i=0; i<3; i++) {
+                    double r1 = (double) setReds[i][2]/setReds[i][1];
+                    double r2 = (double) setReds[i][2]/setReds[i][0];
+
+                    double g1 = (double) setGreens[i][2]/ setGreens[i][1];
+                    double g2 = (double) setGreens[i][2]/ setGreens[i][0];
+
+                    double b1 = (double) setBlues[i][2]/ setBlues[i][1];
+                    double b2 = (double) setBlues[i][2]/ setBlues[i][0];
+
+                    if (r1<=bg+threshold && r1>=bg-threshold
+                        && r2<=br+threshold && r2>=br-threshold) {
+                        red[r][c] = 255;
+                        green[r][c] = 0;
+                        blue[r][c] = 0;
+                    }
+
+                    else if (g1<=bg+threshold && g1>=bg-threshold
+                            && g2<=br+threshold && g2>=br-threshold) {
+                        red[r][c] = 0;
+                        green[r][c] = 255;
+                        blue[r][c] = 0;
+                    }
+
+                    else if (b1<=bg+threshold && b1>=bg-threshold
+                            && b2<=br+threshold && b2>=br-threshold) {
+                        red[r][c] = 0;
+                        green[r][c] = 0;
+                        blue[r][c] = 255;
+                    }
+
+                    else {
+                        red[r][c] = 0;
+                        green[r][c] = 0;
+                        blue[r][c] = 0;
+                    }
+                }
             }
         }
         img.setColorChannels(red, green, blue);
         return img;
     }
 
+//    public DImage refreshImage(DImage img) {
+//        short[][] grid = img.getBWPixelGrid();
+//
+//        for (int r=0; r<grid.length; r++) {
+//            for (int c=0; c<grid[r].length; c++) {
+//
+//            }
+//        }
+//    }
+
     public void mouseClicked(int mouseX, int mouseY, DImage img) {
-        short[][] red = img.getRedChannel();
-        short[][] blue = img.getBlueChannel();
-        short[][] green = img.getGreenChannel();
-        targetRed1 = red[mouseY][mouseX];
-        targetGreen1 = green[mouseY][mouseX];
-        targetBlue1 = blue[mouseY][mouseX];
     }
 
     @Override
     public void keyPressed(char key) {
+        if (key == '+') {
+            threshold+=0.1;
+        }
+
+        if (key == '-') {
+            if (threshold>0.1) {
+                threshold -= 0.1;
+            }
+        }
     }
 
 }
